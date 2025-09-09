78ec352429a8b5912ebf39100dcb8674363f44a0
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-09-09 22:55:21 +0200

    7,8 done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,14 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+        console.log('No contacts to remove.');
+        return;
+    }
+    await writeContacts([]);
+    console.log('All contacts removed successfully.');
+};
 
 removeAllContacts();
