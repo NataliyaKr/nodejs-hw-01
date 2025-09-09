f75e02d45fe9afeda3a1d17a0e052a1570cbe84a
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-09-09 14:23:28 +0200

    addOne

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const contact = await readContacts();
+    const newContact = createFakeContact();
+    const newArray = [...contact, newContact];
+
+    await writeContacts(newArray);
+    console.log('Added one contact', newContact);
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
 
 addOneContact();
